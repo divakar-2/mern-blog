05298573d84dc4e6cad46fe5a47289659d059a16
divakar-02 <divakardivakar9506@gmail.com>
2024-04-04 23:32:25 +0530

    add a middleware and a function to error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
 export const signup=async(req,res)=>{
     const{username,email,password}=req.body;
 
     if(!username||!email||!password || username==='' || email===''||password===''){
-        return res.status(400).json({message:'All fields are required'});
+        next(errorHandler(400,'All fields are required'));
     }
 
     const newUser = new User({
@@ -20,6 +21,6 @@ export const signup=async(req,res)=>{
     res.json('Signup Successfull');
     }
     catch(err){
-        res.status(500).json({message: err.message});
+        next(err);
     }
 }
